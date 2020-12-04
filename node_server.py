344f2b8e101b7582bab89243ac31883ed9b33712
nicolaos.ballas.1@ulaval.ca <nicolaos.ballas.1@ulaval.ca>
2020-12-04 18:44:59 -0500

    refactor

--- node_server.py
+++ node_server.py
@@ -323,5 +323,7 @@ def announce_new_block(block):
                       data=json.dumps(block.__dict__, sort_keys=True),
                       headers=headers)
 
+
 # Uncomment this line if you want to specify the port number in the code
-#app.run(debug=True, port=8000)
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
